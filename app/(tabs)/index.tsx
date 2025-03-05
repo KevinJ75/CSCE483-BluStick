098b96f64114dba8854c48919d89630255b8b6c6
Nicole hernandez <nicolehernandez@Nicoles-MacBook-Pro-2.local>
2025-03-05 17:29:59 -0600

    added log in screen front end

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,74 +1,127 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import * as React from "react";
+import {Image, StyleSheet, Text, View, Pressable} from "react-native";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-
-export default function HomeScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
-}
+const DefaultScreen = () => {
+  	
+  	return (
+    		<View style={styles.defaultScreen}>
+      			<Image style={[styles.bluefind1Icon, styles.frameParentPosition]} resizeMode="cover" source={require('@/assets/images/BlueFind.png')} />
+      			<View style={[styles.frameParent, styles.frameParentPosition]}>
+        				<Pressable style={styles.frame} onPress={()=>{}}>
+          					<Text style={[styles.username, styles.usernameFlexBox]}>Username</Text>
+        				</Pressable>
+        				<View style={[styles.passwordWrapper, styles.wrapperShadowBox]}>
+          					<Text style={[styles.password, styles.logInPosition]}>Password</Text>
+        				</View>
+        				<View style={[styles.logInWrapper, styles.wrapperShadowBox]}>
+          					<Text style={[styles.logIn, styles.logInPosition]}>{`Log In `}</Text>
+        				</View>
+      			</View>
+    		</View>);
+};
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
+  	frameParentPosition: {
+    		width: 256,
+    		left: 78,
+    		position: "absolute"
+  	},
+  	usernameFlexBox: {
+    		opacity: 0.3,
+    		textAlign: "left",
+    		color: "#000"
+  	},
+  	wrapperShadowBox: {
+    		paddingBottom: 21,
+    		paddingRight: 20,
+    		paddingTop: 22,
+    		paddingLeft: 76,
+    		flexDirection: "row",
+    		borderRadius: 10,
+    		shadowOpacity: 1,
+    		elevation: 4,
+    		shadowRadius: 4,
+    		shadowOffset: {
+      			width: 0,
+      			height: 4
+    		},
+    		shadowColor: "rgba(0, 0, 0, 0.25)",
+    		alignSelf: "stretch",
+    		overflow: "hidden"
+  	},
+  	logInPosition: {
+    		zIndex: 0,
+    		top: 22,
+    		fontFamily: "Poppins-Regular",
+    		fontSize: 20,
+    		position: "absolute"
+  	},
+  	bluefind1Icon: {
+    		top: 101,
+    		height: 256
+  	},
+  	username: {
+    		width: 180,
+    		height: 31,
+    		fontFamily: "Poppins-Regular",
+    		fontSize: 20,
+    		opacity: 0.3,
+    		textAlign: "left",
+    		color: "#000"
+  	},
+  	frame: {
+    		paddingTop: 18,
+    		paddingBottom: 18,
+    		paddingLeft: 76,
+    		flexDirection: "row",
+    		borderRadius: 10,
+    		shadowOpacity: 1,
+    		elevation: 4,
+    		shadowRadius: 4,
+    		shadowOffset: {
+      			width: 0,
+      			height: 4
+    		},
+    		shadowColor: "rgba(0, 0, 0, 0.25)",
+    		alignSelf: "stretch",
+    		backgroundColor: "#d9d9d9",
+    		overflow: "hidden"
+  	},
+  	password: {
+    		left: 75,
+    		width: 160,
+    		height: 24,
+    		opacity: 0.3,
+    		textAlign: "left",
+    		color: "#000"
+  	},
+  	passwordWrapper: {
+    		height: 67,
+    		backgroundColor: "#d9d9d9",
+    		paddingRight: 20,
+    		paddingTop: 22
+  	},
+  	logIn: {
+    		left: 99,
+    		color: "#fff",
+    		textAlign: "right",
+    		width: 58
+  	},
+  	logInWrapper: {
+    		backgroundColor: "#60d4ff",
+    		height: 73
+  	},
+  	frameParent: {
+    		top: 450,
+    		gap: 59
+  	},
+  	defaultScreen: {
+    		backgroundColor: "#3eaeef",
+    		flex: 1,
+    		width: "100%",
+    		height: 917,
+    		overflow: "hidden"
+  	}
 });
+
+export default DefaultScreen;
