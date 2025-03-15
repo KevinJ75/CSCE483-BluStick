e14f3b2ae4fc4fdc2e217a47266d64fd3660edbe
Carlos Hernandez <hernandez_2002@tamu.edu>
2025-03-14 21:11:53 -0500

    Revert "Revert "working auth""
    This reverts commit 7f1f07f0a0c1419d755e0658278f4aab64c1c473.

---
+++ app/(auth)/home.tsx
@@ -0,0 +1,14 @@
+import auth from '@react-native-firebase/auth'
+import { View, Text, Button} from 'react-native'
+
+const Page = () => {
+    const user = auth().currentUser;
+
+    return (
+        <View>
+            <Text>Welcome back {user?.email}</Text>
+            <Button title='Sign Out' onPress={() => auth().signOut()} />
+        </View>
+    );
+};
+export default Page;
